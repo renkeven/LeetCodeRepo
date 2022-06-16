2190ce39813b4f4983bf0d4349f56639cbc20634
renkeven <kevenr@student.unimelb.edu.au>
2022-06-17 03:26:24 +1000

    Time: 2630 ms (5.00%), Space: 14.1 MB (48.88%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        just guess how many total symbols there are:
+        ~40 according to google
+        
+        so 52 from alphabet, 10 from numbers, 1 from space, and let's say 38 symbols
+        """
+        
+        if len(s) == 0:
+            return 0
+        
+        window_size = min(101, len(s))
+        
+        def check_unique(inp):
+            if len(inp) == len(set(inp)):
+                return True
+            else:
+                return False
+        
+        while window_size > 1:
+            for i in range(len(s) + 1 - window_size):
+                if check_unique(s[i:i + window_size]):
+                    return window_size
+                
+            window_size -= 1
+                
+        return 1
