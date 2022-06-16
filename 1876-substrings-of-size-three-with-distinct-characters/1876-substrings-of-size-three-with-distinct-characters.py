d0ce059c60f9da7c2a1c924484463035ac4c25a4
renkeven <kevenr@student.unimelb.edu.au>
2022-06-17 03:14:07 +1000

    Time: 53 ms (36.06%), Space: 13.9 MB (57.74%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -1,21 +1,22 @@
+
+
 class Solution:
+    def __init__(self):
+        self.count = 0
+    
     def countGoodSubstrings(self, s: str) -> int:
-        
-        hash_arr = {}
+        """
+        speed it up without storing the cache
+        """
         str_len = len(s)
         
         def check_unique(inp):
-            if inp in hash_arr:
-                hash_arr[inp] += 1
-            else:
-                if len(inp) == len(set(inp)):
-                    hash_arr[inp] = 1
-
+            if len(inp) == len(set(inp)):
+                self.count += 1
             return
-                    
             
         for i in range(str_len - 2):
             check_unique(s[i:i+3])
         
-        return sum(hash_arr.values())
+        return self.count
         
